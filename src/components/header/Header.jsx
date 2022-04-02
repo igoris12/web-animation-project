8ebe4fe6819a0b6f382a3078bb5792cdfd6d4667
Igoris Ivanovas <igoris.x@inbox.lt>
2022-04-02 14:53:32 +0300

    header in mobile mode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 const Header = () => {
   return (
     <header className="header">
-      <p>Igoris Ivanovas</p>
-      <div className="hamburger">icon</div>
+      <h2>Igoris Ivanovas</h2>
+      <div className="hamburger">
+        <GiHamburgerMenu />
+      </div>
       {/* <div className="navigation">navigation here </div> */}
     </header>
   );

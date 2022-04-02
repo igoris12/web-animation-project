b4508a337b31f23a45072d5ca974af6a92ad2f9a
Igoris Ivanovas <igoris.x@inbox.lt>
2022-04-02 14:35:49 +0300

    header and icon setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p>Igoris Ivanovas</p>
+      <div className="hamburger">icon</div>
+      {/* <div className="navigation">navigation here </div> */}
+    </header>
+  );
+};
+
+export default Header;
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../header/Header';
 
 const Page = () => {
-  return <div className="page">Page</div>;
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
 };
 
 export default Page;

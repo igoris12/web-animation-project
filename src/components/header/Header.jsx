056f118ec011068c76d55bbe9a1fb897cccd5f48
Igoris Ivanovas <igoris.x@inbox.lt>
2022-04-05 12:48:04 +0300

    header footer style done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,65 +1,11 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import {
-  BsHouse,
-  BsPerson,
-  BsNewspaper,
-  BsBriefcase,
-  BsLayoutTextWindowReverse,
-  BsLinkedin,
-  BsGithub,
-} from 'react-icons/bs';
-import { AiOutlineMail } from 'react-icons/ai';
-import avatar from '../../images/navigation/avatar.png';
+import HeaderContent from './HeaderContent';
 
 const Header = () => {
   return (
-    <header className="header">
-      <h2>Igoris Ivanovas</h2>
-      <div className="hamburger">
-        <GiHamburgerMenu />
-      </div>
-      <div className="navigation">
-        <div className="image">
-          <img src={avatar} alt="My foto" />
-        </div>
-        <nav className="butList">
-          <a href="#" className="butList-item active">
-            <BsHouse />
-            <p>Home</p>
-          </a>
-          <a href="#" className="butList-item">
-            <BsPerson />
-            <p>About me</p>
-          </a>
-          <a href="#" className="butList-item">
-            <BsNewspaper />
-            <p>Resume</p>
-          </a>
-          <a href="#" className="butList-item">
-            <BsBriefcase />
-            <p>Portfolio</p>
-          </a>
-          <a href="#" className="butList-item">
-            <BsLayoutTextWindowReverse />
-            <p>Blog</p>
-          </a>
-          <a href="#" className="butList-item">
-            <AiOutlineMail />
-            <p>Contact</p>
-          </a>
-        </nav>
-      </div>
-      <ul className="socialsList">
-        <a href="#" className="socialsList-item">
-          <BsLinkedin />
-        </a>
-        <a href="#" className="socialsList-item">
-          <BsGithub />
-        </a>
-        <p>2022/04/01 start</p>
-      </ul>
-    </header>
+    <>
+      <HeaderContent />
+    </>
   );
 };
 

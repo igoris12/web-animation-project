4d437d3628519b60aa82a2c01cf08a905a313f45
Igoris Ivanovas <igoris.x@inbox.lt>
2022-04-06 13:41:08 +0300

    resizing navbar reaction done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,13 @@ import HeaderContent from './HeaderContent';
 const Header = () => {
   const [active, setActive] = useState(false);
 
+  window.addEventListener('resize', (e) => {
+    console.log(e.target.innerWidth);
+    if (e.target.innerWidth > 765) {
+      setActive(true);
+    }
+  });
+
   const activeHandler = () => {
     setActive(!active);
   };

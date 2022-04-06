cd71861326590af2b5cc1af84ee20b99b589702d
Igoris Ivanovas <igoris.x@inbox.lt>
2022-04-06 14:00:40 +0300

    start next step (blog)

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <section className="homeSection">Home</section>;
+};
+
+export default Home;
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Blog from '../Blog/Blog';
 import Header from '../header/Header';
 
 const Page = () => {
   return (
     <div className="page">
       <Header />
+      <Blog />
     </div>
   );
 };

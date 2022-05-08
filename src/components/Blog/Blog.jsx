add1f1c85d5265320264d1b87b3174f83c9b4fbf
Igoris Ivanovas <igoris.x@inbox.lt>
2022-05-08 16:57:58 +0300

    in progress

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Blog = () => {
   return (
     <section className="blog-page">
-      <div className="test1 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vitae
-        obcaecati id exercitationem veritatis vel nobis voluptatum dolores vero!
-        Quisquam, dolore illo necessitatibus fuga aut dolorum sint iste totam
-        nihil quod assumenda laboriosam molestiae ratione reprehenderit
-        voluptate quam sapiente! Dicta eveniet quaerat ipsum. Eligendi ullam
-        dicta ducimus fugiat modi facere recusandae sequi, quod aut mollitia
-        porro vitae officia distinctio, perspiciatis ipsum enim veritatis
-        asperiores repudiandae delectus voluptate omnis amet non, accusamus eum.
-        Doloribus quibusdam consectetur eius voluptatem ex enim dolores
-        voluptates. Atque est quibusdam similique maxime suscipit eligendi
-        dolorem officia, doloribus, pariatur fugit veniam enim doloremque
-        debitis quos nobis eveniet?
+      <div className="test active">
+        11111111111111111111 Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Sed vitae obcaecati id exercitationem veritatis vel nobis
+        voluptatum dolores vero! Quisquam, dolore illo necessitatibus fuga aut
+        dolorum sint iste totam nihil quod assumenda laboriosam molestiae
+        ratione reprehenderit voluptate quam sapiente! Dicta eveniet quaerat
+        ipsum. Eligendi ullam dicta ducimus fugiat modi facere recusandae sequi,
+        quod aut mollitia porro vitae officia distinctio, perspiciatis ipsum
+        enim veritatis asperiores repudiandae delectus voluptate omnis amet non,
+        accusamus eum. Doloribus quibusdam consectetur eius voluptatem ex enim
+        dolores voluptates. Atque est quibusdam similique maxime suscipit
+        eligendi dolorem officia, doloribus, pariatur fugit veniam enim
+        doloremque debitis quos nobis eveniet?
       </div>
-      <div className="test2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vitae
-        obcaecati id exercitationem veritatis vel nobis voluptatum dolores vero!
-        Quisquam, dolore illo necessitatibus fuga aut dolorum sint iste totam
-        nihil quod assumenda laboriosam molestiae ratione reprehenderit
-        voluptate quam sapiente! Dicta eveniet quaerat ipsum. Eligendi ullam
-        dicta ducimus fugiat modi facere recusandae sequi, quod aut mollitia
-        porro vitae officia distinctio, perspiciatis ipsum enim veritatis
-        asperiores repudiandae delectus voluptate omnis amet non, accusamus eum.
-        Doloribus quibusdam consectetur eius voluptatem ex enim dolores
-        voluptates. Atque est quibusdam similique maxime suscipit eligendi
-        dolorem officia, doloribus, pariatur fugit veniam enim doloremque
-        debitis quos nobis eveniet?
+      <div className="test">
+        22222222222222222222222 Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sed vitae obcaecati id exercitationem veritatis vel
+        nobis voluptatum dolores vero! Quisquam, dolore illo necessitatibus fuga
+        aut dolorum sint iste totam nihil quod assumenda laboriosam molestiae
+        ratione reprehenderit voluptate quam sapiente! Dicta eveniet quaerat
+        ipsum. Eligendi ullam dicta ducimus fugiat modi facere recusandae sequi,
+        quod aut mollitia porro vitae officia distinctio, perspiciatis ipsum
+        enim veritatis asperiores repudiandae delectus voluptate omnis amet non,
+        accusamus eum. Doloribus quibusdam consectetur eius voluptatem ex enim
+        dolores voluptates. Atque est quibusdam similique maxime suscipit
+        eligendi dolorem officia, doloribus, pariatur fugit veniam enim
+        doloremque debitis quos nobis eveniet?
       </div>
     </section>
   );

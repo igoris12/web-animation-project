15f0b8f3fe06ab9273e172e353a1a88614122bb5
Igoris Ivanovas <igoris.x@inbox.lt>
2022-05-15 14:10:31 +0300

    working on text animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,11 @@ const Home = () => {
           Igoris <span>Ivanovas</span>
         </h1>
         <div className="title">
-          I am a <span className="animation"></span>
+          I am a{' '}
+          <span className="animatedText">
+            <b className="visible">Web developer</b>
+            <b className="hidden">Freelancer</b>
+          </span>
         </div>
       </div>
     </section>

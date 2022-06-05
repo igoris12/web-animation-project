111be7f4a457be5af7f389535afdcabbe94aeeac
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-05 17:36:58 +0300

    layout bug fix and hiding options function done

--- src/components/ThemeOptions/ThemeOptions.jsx
+++ src/components/ThemeOptions/ThemeOptions.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaCog } from 'react-icons/fa';
 
 function ThemeOptions() {
+  const [active, setActive] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setActive(false);
+    }, 2000);
+  }, []);
+
+  const toggleOptions = () => {
+    setActive(!active);
+  };
+
   return (
-    <div className="ThemeOptions">
-      <span className="icon">
+    <div className={active ? 'ThemeOptions' : 'ThemeOptions hiden'}>
+      <span className="icon" onClick={toggleOptions}>
         <FaCog />
       </span>
       <div className="optionsContent">
@@ -15,39 +27,15 @@ function ThemeOptions() {
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
         </select>
         <h4>Colors</h4>
         <ul>
-          <span style={{ 'background-color': '#00A3E1' }}></span>
-          <span style={{ 'background-color': '#E82A2A' }}></span>
-          <span style={{ 'background-color': '#6ac045' }}></span>
-          <span style={{ 'background-color': '#D1A71D' }}></span>
-          <span style={{ 'background-color': '#FF1493' }}></span>
-          <span style={{ 'background-color': '#5078FF' }}></span>
+          <span style={{ backgroundColor: '#00A3E1' }}></span>
+          <span style={{ backgroundColor: '#E82A2A' }}></span>
+          <span style={{ backgroundColor: '#6ac045' }}></span>
+          <span style={{ backgroundColor: '#D1A71D' }}></span>
+          <span style={{ backgroundColor: '#FF1493' }}></span>
+          <span style={{ backgroundColor: '#5078FF' }}></span>
         </ul>
       </div>
     </div>

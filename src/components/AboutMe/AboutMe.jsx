9df2adc9e7b7fc469ba950c94a0b83258c8d25b9
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-09 19:35:28 +0300

    about section layout

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -14,7 +14,54 @@ const AboutMe = () => {
           </span>
         </h1>
       </div>
-      <div>next</div>
+      <div className="about row">
+        <div>
+          <h2>UI/UX Developer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            corporis doloremque facere in porro quaerat? Dolore quibusdam saepe
+            magnam sequi aliquid consectetur voluptates animi et, illum
+            exercitationem ad? Totam, eaque error commodi fuga quia velit nobis!
+            Blanditiis, laudantium rem. Architecto modi ut, non atque neque
+            tempora ipsum numquam commodi ex?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            corporis doloremque facere in porro quaerat? Dolore quibusdam saepe
+            magnam sequi aliquid consectetur voluptates animi et, illum
+            exercitationem ad? Totam, eaque error commodi fuga quia velit nobis!
+            Blanditiis, laudantium rem. Architecto modi ut, non atque neque
+            tempora ipsum numquam commodi ex?
+          </p>
+          <img src="#" alt="signature" />
+        </div>
+        <aside>
+          <h2>Personal information</h2>
+          <ul className="infoList">
+            <li className="item">
+              <span className="title">Name</span>
+              <p className="value">Igoris Ivanovas</p>
+            </li>
+            <li className="item">
+              <span className="title">Age26</span>
+              <p className="value">22 Years</p>
+            </li>
+            <li className="item">
+              <span className="title">Residence</span>
+              <p className="value">Lithuania</p>
+            </li>
+            <li className="item">
+              <span className="title">Email</span>
+              <p className="value">igoris.x@inbox.lt</p>
+            </li>
+            <li className="item">
+              <span className="title">Freelance</span>
+              <p className="value">Available</p>
+            </li>
+          </ul>
+          <button>Download resume</button>
+        </aside>
+      </div>
     </section>
   );
 };

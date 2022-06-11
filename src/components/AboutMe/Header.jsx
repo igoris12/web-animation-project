3ef0ca2650aff5bd24a46e1ac2b82edadc1688f2
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-11 19:27:41 +0300

    AboutMe main header layout done

--- src/components/AboutMe/Header.jsx
+++ src/components/AboutMe/Header.jsx
@@ -4,13 +4,15 @@ import { HiOutlineUser } from 'react-icons/hi';
 const Header = () => {
   const { optionsState } = useContext(OptionsContext);
   return (
-    <div className="sectionHeader container">
-      <h1 style={{ color: optionsState.color }}>
-        About me.
-        <span>
-          <HiOutlineUser />
-        </span>
-      </h1>
+    <div className="sectionHeader container-fluid">
+      <div className="row">
+        <h1 className="col-12" style={{ color: optionsState.color }}>
+          About me.
+          <span>
+            <HiOutlineUser />
+          </span>
+        </h1>
+      </div>
     </div>
   );
 };

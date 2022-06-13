e1ab1b474fd59cdcb93cacb83a65001d187286e5
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-13 19:26:14 +0300

    Services component done

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import About from './About';
+import Services from './Services';
 const AboutMe = () => {
   return (
     <section id="aboutMe">
       <Header />
       <About />
+      <Services />
     </section>
   );
 };

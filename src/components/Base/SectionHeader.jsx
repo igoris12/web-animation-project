bfccfbcddf6968107729c0437b37997557ce27bd
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-14 19:10:26 +0300

    SectionHeader style done

--- src/components/Base/SectionHeader.jsx
+++ src/components/Base/SectionHeader.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import OptionsContext from '../../reducers/OptionsContext';
 
-const SectionHeader = ({ text, col = '' }) => {
+const SectionHeader = ({ text }) => {
+  const { optionsState } = useContext(OptionsContext);
+
   return (
-    <h2 className={'SectionHeader' + ' ' + col}>
-      {text}
-      <span className="left"></span>
-      <span className="right"></span>
-    </h2>
+    <div className={'SectionHeaderWrapper'}>
+      <h2
+        className={'SectionHeader'}
+        style={{ borderColor: optionsState.color }}
+      >
+        {text}
+        <span
+          className="left"
+          style={{ background: optionsState.color }}
+        ></span>
+        <span
+          className="right"
+          style={{ background: optionsState.color }}
+        ></span>
+      </h2>
+    </div>
   );
 };
 

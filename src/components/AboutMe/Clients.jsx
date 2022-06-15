25208b2d7d7f9113d7699bf6b452dcceb09435b5
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-15 15:17:55 +0300

    swiper setup

--- src/components/AboutMe/Clients.jsx
+++ src/components/AboutMe/Clients.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import SectionHeader from '../Base/SectionHeader';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 const Clients = () => {
   return (
     <section className="Clients container-fluid">
       <div className="row">
         <SectionHeader text={'Clients'} />
       </div>
+      <Swiper spaceBetween={50} slidesPerView={3} loop={true}>
+        <SwiperSlide>
+          <div>
+            <img src="#" alt="logo" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src="#" alt="logo" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src="#" alt="logo" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src="#" alt="logo" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src="#" alt="logo" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };

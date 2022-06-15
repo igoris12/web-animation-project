b42872597bf4cd4d43f441ff13a5ebdf5e28a682
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-15 15:04:36 +0300

    Clients component setup

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,12 +3,14 @@ import Header from '../Base/Header';
 import { HiOutlineUser } from 'react-icons/hi';
 import About from './About';
 import Services from './Services';
+import Clients from './Clients';
 const AboutMe = () => {
   return (
     <section id="aboutMe">
       <Header text={'About Me.'} icon={<HiOutlineUser />} />
       <About />
       <Services />
+      <Clients />
     </section>
   );
 };
---
+++ src/components/AboutMe/Clients.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Clients = () => {
+  return <section className="Clients">Clients</section>;
+};
+
+export default Clients;

3ac95bf621e79f086bcb32bb9966032c2f584f19
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-15 15:09:16 +0300

    Clients sectionHeader done

--- src/components/AboutMe/Clients.jsx
+++ src/components/AboutMe/Clients.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SectionHeader from '../Base/SectionHeader';
 
 const Clients = () => {
-  return <section className="Clients">Clients</section>;
+  return (
+    <section className="Clients container-fluid">
+      <div className="row">
+        <SectionHeader text={'Clients'} />
+      </div>
+    </section>
+  );
 };
 
 export default Clients;

79ac163212a17b92be3393e9d11bcf93af10d5c8
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-21 18:32:42 +0300

    TimeStampElement markup

--- src/components/Resume/TimeStampElement.jsx
+++ src/components/Resume/TimeStampElement.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const TimeStampElement = () => {
-  return <div className="TimeStampElement">TimeStampElement</div>;
+  return (
+    <div className="TimeStampElement">
+      <spam className="years">
+        <span className="from">99</span>
+        <span className="to">100</span>
+        <span className="line"></span>
+      </spam>
+      <span className="content">
+        <h4>header</h4>
+        <h5>header nr2</h5>
+        <p className="info">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius,
+          accusamus.
+        </p>
+      </span>
+    </div>
+  );
 };
 
 export default TimeStampElement;

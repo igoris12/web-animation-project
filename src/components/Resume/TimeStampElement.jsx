c88e8441ce60f2f6d87e4cd0a500c29cc5657a27
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-21 19:32:13 +0300

    TimeStampElement done

--- src/components/Resume/TimeStampElement.jsx
+++ src/components/Resume/TimeStampElement.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const TimeStampElement = () => {
+const TimeStampElement = ({
+  text = 'info text',
+  title = 'title',
+  location = 'location',
+  from,
+  to,
+}) => {
   return (
     <div className="TimeStampElement">
-      <span className="to sectionParagraph">2022</span>
-      <span className="from sectionParagraph">2021</span>
+      <span className="to sectionParagraph">{to}</span>
+      <span className="from sectionParagraph">{from}</span>
       <span className="line" style={{ backgroundColor: '#007aff' }}>
         <span className="circleTop" style={{ borderColor: '#007aff' }}></span>
         <span
@@ -13,12 +19,9 @@ const TimeStampElement = () => {
         ></span>
       </span>
       <span className="content">
-        <h4 className="sectionSecondaryHeader">header</h4>
-        <h5>header nr2</h5>
-        <p className="info sectionParagraph">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius,
-          accusamus.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </p>
+        <h4 className="sectionSecondaryHeader">{title}</h4>
+        <h5>{location}</h5>
+        <p className="info sectionParagraph">{text}</p>
       </span>
     </div>
   );

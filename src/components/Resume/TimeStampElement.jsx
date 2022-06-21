ac8a260c8f34c7c34493af09918fa8ce059e5bdf
Igoris Ivanovas <igoris.x@inbox.lt>
2022-06-21 19:38:57 +0300

    TimeStampElement add color change option

--- src/components/Resume/TimeStampElement.jsx
+++ src/components/Resume/TimeStampElement.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import OptionsContext from '../../reducers/OptionsContext';
 
 const TimeStampElement = ({
   text = 'info text',
@@ -7,20 +8,24 @@ const TimeStampElement = ({
   from,
   to,
 }) => {
+  const { optionsState } = useContext(OptionsContext);
   return (
     <div className="TimeStampElement">
       <span className="to sectionParagraph">{to}</span>
       <span className="from sectionParagraph">{from}</span>
-      <span className="line" style={{ backgroundColor: '#007aff' }}>
-        <span className="circleTop" style={{ borderColor: '#007aff' }}></span>
+      <span className="line" style={{ backgroundColor: optionsState.color }}>
+        <span
+          className="circleTop"
+          style={{ borderColor: optionsState.color }}
+        ></span>
         <span
           className="circleBottom"
-          style={{ borderColor: '#007aff' }}
+          style={{ borderColor: optionsState.color }}
         ></span>
       </span>
       <span className="content">
         <h4 className="sectionSecondaryHeader">{title}</h4>
-        <h5>{location}</h5>
+        <h5 style={{ color: optionsState.color }}>{location}</h5>
         <p className="info sectionParagraph">{text}</p>
       </span>
     </div>

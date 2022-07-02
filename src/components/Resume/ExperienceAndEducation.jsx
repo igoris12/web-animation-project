5c48269b93563e32a5a43c725c9e324e091a7de2
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-02 18:27:11 +0300

    add section headers and scroll mode

--- src/components/Resume/ExperienceAndEducation.jsx
+++ src/components/Resume/ExperienceAndEducation.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import TimeStampElement from './TimeStampElement';
+import SectionHeader from '../Base/SectionHeader';
 
 const experienceAndEducation = () => {
   return (
     <>
       <div className="experienceEducation container-fluid">
         <div className="experience row">
+          <SectionHeader text={'experience'} />
           <ul className="col-12">
+            <TimeStampElement />
             <TimeStampElement />
           </ul>
         </div>
         <div className="education row">
-          <ul className="col-12">content in progress</ul>
+          <SectionHeader text={'education'} />
+          <ul className="col-12 ">
+            <TimeStampElement />
+            <TimeStampElement />
+          </ul>
         </div>
       </div>
     </>

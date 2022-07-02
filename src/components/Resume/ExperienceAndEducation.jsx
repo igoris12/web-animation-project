a57ceffc88cd37e2f3203d7910e56036d1ae7c12
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-02 19:06:21 +0300

    education section content done

--- src/components/Resume/ExperienceAndEducation.jsx
+++ src/components/Resume/ExperienceAndEducation.jsx
@@ -18,8 +18,20 @@ const experienceAndEducation = () => {
           <div className="education col-12 col-lg-5">
             <SectionHeader text={'education'} />
             <ul>
-              <TimeStampElement />
-              <TimeStampElement />
+              <TimeStampElement
+                from="2021"
+                to="2022"
+                title="self Learning"
+                location="around the world"
+                text="After full-stack courses I decide to learn more advanced technology in front-end like React.js."
+              />
+              <TimeStampElement
+                title="Full-Stack Developer"
+                location={'Baltic institute of technology, Vilnius, Lithuania'}
+                text="Gain a deep understanding of how to build, deploy, secure and scale programs and build expertise across the user interface, business logic and database stacks."
+                from="2020"
+                to="2021"
+              />
             </ul>
           </div>
         </div>

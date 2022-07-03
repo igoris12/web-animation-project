f2b68d3612c9560e986bc22a09cf0437c59fec79
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-03 21:07:39 +0300

    Skills section setup

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../Base/Header';
 import { BsNewspaper } from 'react-icons/bs';
 import ExperienceAndEducation from './ExperienceAndEducation';
+import Skills from './Skills';
 const Resume = () => {
   return (
     <section className="resume">
       <Header text="My resume." icon={<BsNewspaper />} />
       <ExperienceAndEducation />
+      <Skills />
     </section>
   );
 };
---
+++ src/components/Resume/Skills.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Skills = () => {
+  return <section className="skills container-fluid">Skills</section>;
+};
+
+export default Skills;

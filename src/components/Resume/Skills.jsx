10fafa0394d62969e45d7601579cba953c6d52e4
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-09 20:13:43 +0300

    Skills layout

--- src/components/Resume/Skills.jsx
+++ src/components/Resume/Skills.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import SectionHeader from '../Base/SectionHeader';
 
 const Skills = () => {
-  return <section className="skills container-fluid">Skills</section>;
+  return (
+    <section className="skills container-fluid">
+      <div className="row">
+        <div className="design col-12 col-lg-6">
+          <SectionHeader text="Design skills" />
+        </div>
+        <div className="coding col-12 col-lg-6">
+          <SectionHeader text="Coding skills" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;

a23b82462556e774e8d9b25b89ab986a823b26e2
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-09 20:28:37 +0300

    SkillsItem setup and layout

--- src/components/Resume/Skills.jsx
+++ src/components/Resume/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SectionHeader from '../Base/SectionHeader';
+import SkillsItem from './SkillsItem';
 
 const Skills = () => {
   return (
@@ -7,9 +8,19 @@ const Skills = () => {
       <div className="row">
         <div className="design col-12 col-lg-6">
           <SectionHeader text="Design skills" />
+          <ul>
+            <SkillsItem title="Coral Draw" value={91} />
+            <SkillsItem title="Photoshop" value={82} />
+            <SkillsItem title="illustrator" value={94} />
+          </ul>
         </div>
         <div className="coding col-12 col-lg-6">
           <SectionHeader text="Coding skills" />
+          <ul>
+            <SkillsItem title="HTML 5" value={83} />
+            <SkillsItem title="CSS3" value={75} />
+            <SkillsItem title="Javascript" value={92} />
+          </ul>
         </div>
       </div>
     </section>
---
+++ src/components/Resume/SkillsItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SkillsItem = ({ title, value }) => {
+  return (
+    <div className="SkillsItem">
+      <h4 className="title">{title}</h4>
+      <progress value={value} max="100">
+        <span>{value}</span>
+      </progress>
+    </div>
+  );
+};
+
+export default SkillsItem;

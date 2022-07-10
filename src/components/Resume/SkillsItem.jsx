2ebfaf2e03ccc52366916325b808e2ea6eda198e
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-10 15:00:12 +0300

    done mian pregressbar layout

--- src/components/Resume/SkillsItem.jsx
+++ src/components/Resume/SkillsItem.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 
 const SkillsItem = ({ title, value }) => {
+  const [skillsAnimation, setSkillAnimation] = useState(0);
+  useEffect(() => {
+    window.addEventListener('scroll', (event) => {
+      console.log('Scrolling...');
+    });
+  }, []);
+
   return (
     <div className="SkillsItem">
       <h4 className="title sectionSecondaryHeader">{title}</h4>
-      <progress style={{ background: 'red' }} value={value} max="100">
-        {value}
-      </progress>
+      <div className="pregressBar">
+        <span
+          className="progress"
+          style={{ right: `calc(100% - ${skillsAnimation}%)` }}
+        ></span>
+        <span className="value">{value}</span>
+      </div>
     </div>
   );
 };

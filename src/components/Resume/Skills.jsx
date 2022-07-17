b79b4b8088c6f8c4f29ae7019cc3e7fb654bad96
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-17 17:38:59 +0300

    Make progress animation individual

--- src/components/Resume/Skills.jsx
+++ src/components/Resume/Skills.jsx
@@ -1,30 +1,25 @@
 import React, { useEffect } from 'react';
 import SectionHeader from '../Base/SectionHeader';
 import SkillsItem from './SkillsItem';
-import { useInView } from 'react-intersection-observer';
 
 const Skills = () => {
-  const { ref, inView, entry } = useInView({
-    threshold: 0,
-  });
-
   return (
-    <section ref={ref} className="skills container-fluid">
+    <section className="skills container-fluid">
       <div className="row">
         <div className="design col-12 col-lg-6">
           <SectionHeader text="Design skills" />
           <ul>
-            <SkillsItem title="Coral Draw" value={91} inView={inView} />
-            <SkillsItem title="Photoshop" value={82} inView={inView} />
-            <SkillsItem title="illustrator" value={94} inView={inView} />
+            <SkillsItem title="Coral Draw" value={91} />
+            <SkillsItem title="Photoshop" value={82} />
+            <SkillsItem title="illustrator" value={94} />
           </ul>
         </div>
         <div className="coding col-12 col-lg-6">
           <SectionHeader text="Coding skills" />
           <ul>
-            <SkillsItem title="HTML 5" value={83} inView={inView} />
-            <SkillsItem title="CSS3" value={75} inView={inView} />
-            <SkillsItem title="Javascript" value={92} inView={inView} />
+            <SkillsItem title="HTML 5" value={83} />
+            <SkillsItem title="CSS3" value={75} />
+            <SkillsItem title="Javascript" value={92} />
           </ul>
         </div>
       </div>
--- src/components/Resume/SkillsItem.jsx
+++ src/components/Resume/SkillsItem.jsx
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import React from 'react';
+import OptionsContext from '../../reducers/OptionsContext';
+import { useInView } from 'react-intersection-observer';
 
-const SkillsItem = ({ title, value, inView }) => {
+const SkillsItem = ({ title, value }) => {
+  const { optionsState } = useContext(OptionsContext);
   const [skillsAnimation, setSkillAnimation] = useState(0);
+
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+  });
+
   useEffect(() => {
     if (inView) setSkillAnimation(value);
   }, [inView]);
 
   return (
-    <div className="SkillsItem">
+    <div ref={ref} className="SkillsItem">
       <h4 className="title sectionSecondaryHeader">{title}</h4>
       <div className="pregressBar">
         <span
           className="progress"
-          style={{ right: `calc(100% - ${skillsAnimation}%)` }}
+          style={{
+            right: `calc(100% - ${skillsAnimation}%)`,
+            backgroundColor: optionsState.color,
+          }}
         ></span>
         <span className="value">{value}</span>
       </div>

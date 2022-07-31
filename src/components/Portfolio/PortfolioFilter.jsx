d08ba7e29e7e5b207106388d1b44bdb150b2d7da
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-31 17:36:44 +0300

    active filter switcher done

--- src/components/Portfolio/PortfolioFilter.jsx
+++ src/components/Portfolio/PortfolioFilter.jsx
@@ -1,33 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import OptionsContext from '../../reducers/OptionsContext';
 const PortfolioFilter = () => {
   const { optionsState } = useContext(OptionsContext);
+  const [active, setActive] = useState(0);
+  const filtering = (filterNum, item = 'all') => {
+    if (filterNum !== active) {
+      setActive(filterNum);
+    }
+  };
   return (
     <div className="portfolioFilter container-fluid">
       <div className="row">
         <div className="col-12">
           <ul>
             <li
-              style={{ background: optionsState.color }}
-              className="sectionParagraph active"
+              //   style={{ background: optionsState.color }}
+              className={
+                active === 0 ? 'sectionParagraph active' : 'sectionParagraph'
+              }
+              onClick={() => filtering(0)}
             >
               All
             </li>
             <li
-              style={{ background: optionsState.color }}
-              className="sectionParagraph active"
+              //   style={{ background: optionsState.color }}
+              className={
+                active === 1 ? 'sectionParagraph active' : 'sectionParagraph'
+              }
+              onClick={() => filtering(1)}
             >
               React
             </li>
             <li
-              style={{ background: optionsState.color }}
-              className="sectionParagraph active"
+              //   style={{ background: optionsState.color }}
+              className={
+                active === 2 ? 'sectionParagraph active' : 'sectionParagraph'
+              }
+              onClick={() => filtering(2)}
             >
               JavaScript
             </li>
             <li
-              style={{ background: optionsState.color }}
-              className="sectionParagraph active"
+              //   style={{ background: optionsState.color }}
+              className={
+                active === 3 ? 'sectionParagraph active' : 'sectionParagraph'
+              }
+              onClick={() => filtering(3)}
             >
               Sass
             </li>

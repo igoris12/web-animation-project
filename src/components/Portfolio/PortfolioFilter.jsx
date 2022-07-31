e887ce881901d653d4a5d9525344c8ca6387c7ac
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-31 17:17:51 +0300

    list style and layout

--- src/components/Portfolio/PortfolioFilter.jsx
+++ src/components/Portfolio/PortfolioFilter.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import OptionsContext from '../../reducers/OptionsContext';
 const PortfolioFilter = () => {
+  const { optionsState } = useContext(OptionsContext);
   return (
     <div className="portfolioFilter container-fluid">
       <div className="row">
         <div className="col-12">
           <ul>
-            <li>All</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Sass</li>
+            <li
+              style={{ background: optionsState.color }}
+              className="sectionParagraph active"
+            >
+              All
+            </li>
+            <li
+              style={{ background: optionsState.color }}
+              className="sectionParagraph active"
+            >
+              React
+            </li>
+            <li
+              style={{ background: optionsState.color }}
+              className="sectionParagraph active"
+            >
+              JavaScript
+            </li>
+            <li
+              style={{ background: optionsState.color }}
+              className="sectionParagraph active"
+            >
+              Sass
+            </li>
           </ul>
         </div>
       </div>

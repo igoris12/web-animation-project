5b804d899096dc6bba87ddc7e032de4a9c1b67a4
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-31 16:53:40 +0300

    PortfolioFilter setup and layout

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Base/Header';
 import { BsBriefcase } from 'react-icons/bs';
+import PortfolioFilter from './PortfolioFilter';
 const Portfolio = () => {
   return (
     <section className="portfolio">
       <Header text="Portfolio." icon={<BsBriefcase />} />
+      <PortfolioFilter />
     </section>
   );
 };
---
+++ src/components/Portfolio/PortfolioFilter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PortfolioFilter = () => {
+  return (
+    <div className="portfolioFilter container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <ul>
+            <li>All</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Sass</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioFilter;

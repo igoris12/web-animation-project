d95b3b7c15553231df387e7172d8666fa0bdbcfc
Igoris Ivanovas <igoris.x@inbox.lt>
2022-07-31 16:32:58 +0300

    Portfolio header done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Header from '../Base/Header';
+import { BsBriefcase } from 'react-icons/bs';
 const Portfolio = () => {
-  return <section className="portfolio">Portfolio</section>;
+  return (
+    <section className="portfolio">
+      <Header text="Portfolio." icon={<BsBriefcase />} />
+    </section>
+  );
 };
 
 export default Portfolio;

11f2a1572b84d1e350ead08c1a3c504c3c371f32
Igoris Ivanovas <igoris.x@inbox.lt>
2022-09-15 12:04:13 +0300

    delete demo gallery

--- src/components/Portfolio/js/filter.js
+++ src/components/Portfolio/js/filter.js
@@ -1,23 +1,23 @@
 import $ from 'jquery';
 import Isotope from 'isotope-layout';
 
-$(document).ready(function () {
-    let grid = document.querySelector('.grid');
-    let iso = new Isotope(grid, {
-        itemSelector: '.element-item',
-        layoutMode: 'masonry'
-    });
+// $(document).ready(function () {
+//     let grid = document.querySelector('.grid');
+//     let iso = new Isotope(grid, {
+//         itemSelector: '.element-item',
+//         layoutMode: 'masonry'
+//     });
 
-    let filtersElem = document.querySelector('.portfolioFilterList');
-    filtersElem.addEventListener('click', function (event) {
-        if (event.target.localName !== "li") {
-            return;
-        }
+//     let filtersElem = document.querySelector('.portfolioFilterList');
+//     filtersElem.addEventListener('click', function (event) {
+//         if (event.target.localName !== "li") {
+//             return;
+//         }
 
-        let filterValue = event.target.getAttribute('data-filter');
-        console.log(filterValue);
+//         let filterValue = event.target.getAttribute('data-filter');
+       
 
-        iso.arrange({ filter: filterValue });
-    });
-});
+//         iso.arrange({ filter: filterValue });
+//     });
+// });
 

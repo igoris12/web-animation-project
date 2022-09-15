067e499a48387474349f59af7d17bffab6c13d97
Igoris Ivanovas <igoris.x@inbox.lt>
2022-09-15 12:02:40 +0300

    worki on demo

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,50 +9,6 @@ const Portfolio = () => {
       <Header text="Portfolio." icon={<BsBriefcase />} />
       {/* <PortfolioFilter /> */}
       {/* <Gallery /> */}
-
-      <div className="portfolioFilter container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <ul className="portfolioFilterList">
-              <li className="active" data-filter="*">
-                All
-              </li>
-              <li data-filter=".Odd">Odd</li>
-              <li data-filter=".Even">Even</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      {/*  */}
-      <div className=" container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="grid container-fluid">
-              <div data-category="odd" className="element-item Odd">
-                <h3>1</h3>
-              </div>
-              <div data-category="even" className="element-item Even">
-                <h3>2</h3>
-              </div>
-              <div data-category="odd" className="element-item Odd">
-                <h3>3</h3>
-              </div>
-              <div data-category="even" className="element-item Even">
-                <h3>4</h3>
-              </div>
-              <div data-category="odd" className="element-item Odd">
-                <h3>5</h3>
-              </div>
-              <div data-category="even" className="element-item Even">
-                <h3>6</h3>
-              </div>
-              <div data-category="odd" className="element-item Odd">
-                <h3>7</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
--- src/components/Portfolio/js/filter.js
+++ src/components/Portfolio/js/filter.js
@@ -1,9 +1,23 @@
-import Isotope from "isotope-layout";
-var grid = document.querySelector('.grid');
-var iso = new Isotope(grid, {
-    // options...
-    itemSelector: '.grid-item',
-    masonry: {
-        columnWidth: 200
-    }
+import $ from 'jquery';
+import Isotope from 'isotope-layout';
+
+$(document).ready(function () {
+    let grid = document.querySelector('.grid');
+    let iso = new Isotope(grid, {
+        itemSelector: '.element-item',
+        layoutMode: 'masonry'
+    });
+
+    let filtersElem = document.querySelector('.portfolioFilterList');
+    filtersElem.addEventListener('click', function (event) {
+        if (event.target.localName !== "li") {
+            return;
+        }
+
+        let filterValue = event.target.getAttribute('data-filter');
+        console.log(filterValue);
+
+        iso.arrange({ filter: filterValue });
+    });
 });
+

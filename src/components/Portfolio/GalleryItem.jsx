1dcc94b79eb38c9cce25c0d53c681c7bf573b292
Igoris Ivanovas <igoris.x@inbox.lt>
2022-09-25 21:00:21 +0300

    working on glow effect

--- src/components/Portfolio/GalleryItem.jsx
+++ src/components/Portfolio/GalleryItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import OptionsContext from '../../reducers/OptionsContext';
 import {
   BsImage,
@@ -35,32 +35,44 @@ function GalleryItem({ project }) {
     }
   };
 
- let tools = '';
-
+  let tools = '';
+  const [cursorPoint ,setCursorPoint] = useState({x: 20, y:20});
   project.tools.forEach(element => {
     tools += element + ' ';
-});
+  });
+ 
+  const glowHandeler = (e) => {
 
+    setCursorPoint({x: e.clientX - 300, y: e.clientY - 250 })
+  console.log(e);
+  }
 
   return (
     <li
+    onMouseMove={glowHandeler}
     className={'galleryItem '  + tools + project.layout }
     >
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
-        <span class="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
       <img src={getImage(project.img)} alt="Project" />
       <span
+      
         className="hoverEffect"
         style={{ backgroundColor: optionsState.color }}
       >
       </span>
+      <div 
+       data-x={cursorPoint.x}
+       data-y={cursorPoint.y}
+       style={{top: cursorPoint.y - 150 + 'px', left: cursorPoint.x - 150 + 'px'}}
+      className="glowEffect"></div>
       <h2 className="sectionSecondaryHeader">{project.title}</h2>
       <span className="tools sectionParagraph ">
         {project.tools.map((data, index) => (

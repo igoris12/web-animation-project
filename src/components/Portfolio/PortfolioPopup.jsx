9bce93d479a7a6a2ee2a871799bb37848244a34c
Igoris Ivanovas <igoris.x@inbox.lt>
2022-11-21 17:57:45 +0200

    popup content first part  basic layout

--- src/components/Portfolio/PortfolioPopup.jsx
+++ src/components/Portfolio/PortfolioPopup.jsx
@@ -11,7 +11,17 @@ function PortfolioPopup({data = {}}) {
   return (
     <div className={show === true ? 'portfolioPopup active' : 'portfolioPopup'}>
       <div className="header"><RiCloseCircleFill onClick={closeFunction}/></div>
-       some text for test {data.title}
+      <div className="content">
+        <h4 className="title sectionSecondaryHeader">Project name</h4>
+        <p className="description sectionParagraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis veniam repellendus ex? Cum accusantium ea qui maiores molestias eaque, nihil quasi ad reiciendis quis assumenda corporis eius ipsam eum inventore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum incidunt in fuga at reprehenderit labore, quam aliquam libero natus magni!</p>
+        <div className="info">
+          <ul>
+            <li><span>Client:</span></li>
+            <li><span>Website:</span></li>
+            <li><span>Tools:</span></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }

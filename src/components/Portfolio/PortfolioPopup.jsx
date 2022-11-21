4980ed062b544ba4647defc7a64f0e99829bd888
Igoris Ivanovas <igoris.x@inbox.lt>
2022-11-21 18:31:20 +0200

    responsive content layout done

--- src/components/Portfolio/PortfolioPopup.jsx
+++ src/components/Portfolio/PortfolioPopup.jsx
@@ -10,7 +10,7 @@ function PortfolioPopup({data = {}}) {
 
   return (
     <div className={show === true ? 'portfolioPopup active' : 'portfolioPopup'}>
-      <div className="header"><RiCloseCircleFill onClick={closeFunction}/></div>
+      <div className="popUpheader"><RiCloseCircleFill onClick={closeFunction}/></div>
       <div className="content">
         <h4 className="title sectionSecondaryHeader">Project name</h4>
         <p className="description sectionParagraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis veniam repellendus ex? Cum accusantium ea qui maiores molestias eaque, nihil quasi ad reiciendis quis assumenda corporis eius ipsam eum inventore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum incidunt in fuga at reprehenderit labore, quam aliquam libero natus magni!</p>

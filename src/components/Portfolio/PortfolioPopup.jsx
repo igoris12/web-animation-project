e19f48510a77fdfc65ffd3f8d77279bf5ab41748
Igoris Ivanovas <igoris.x@inbox.lt>
2022-11-22 18:13:00 +0200

    dynamic color in info component done, open function done

--- src/components/Portfolio/PortfolioPopup.jsx
+++ src/components/Portfolio/PortfolioPopup.jsx
@@ -1,25 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import { RiCloseCircleFill } from 'react-icons/ri';
 import img1 from '../../images/Portfolio/Projects/snakeGame.png';
+import OptionsContext from '../../reducers/OptionsContext';
 
-function PortfolioPopup({data = {}}) {
-  const [show, setShow] = useState(true);
-
-  const closeFunction = () => {
-    setShow(false)
-  }
+function PortfolioPopup({data = {}, showPopup, closeFunction}) {
+  const { optionsState } = useContext(OptionsContext);
+  
 
   return (
-    <div className={show === true ? 'portfolioPopup active' : 'portfolioPopup'}>
+    <div className={showPopup === true ? 'portfolioPopup active' : 'portfolioPopup'}>
       <div className="popUpheader"><RiCloseCircleFill onClick={closeFunction}/></div>
       <div className="content">
         <h4 className="title sectionSecondaryHeader">Project name</h4>
         <p className="description sectionParagraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis veniam repellendus ex? Cum accusantium ea qui maiores molestias eaque, nihil quasi ad reiciendis quis assumenda corporis eius ipsam eum inventore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum incidunt in fuga at reprehenderit labore, quam aliquam libero natus magni!</p>
         <div className="info">
           <ul>
-            <li><span>Client: </span> Themeforest</li>
-            <li><span>Website: </span> envato.com</li>
-            <li><span>Tools: </span> web, creative, photography</li>
+            <li><span style={{color: optionsState.color}}>Client: </span> Themeforest</li>
+            <li><span style={{color: optionsState.color}}>Website: </span> envato.com</li>
+            <li><span style={{color: optionsState.color}}>Tools: </span> web, creative, photography</li>
           </ul>
         </div>
         <ul className="imagis">

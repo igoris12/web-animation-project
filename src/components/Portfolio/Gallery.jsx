090674472a01bcb42c3d2131220937b82eb474a0
Igoris Ivanovas <igoris.x@inbox.lt>
2022-11-26 17:50:13 +0200

    Video showcase layout done

--- src/components/Portfolio/Gallery.jsx
+++ src/components/Portfolio/Gallery.jsx
@@ -4,10 +4,13 @@ import GalleryDataContext from '../../reducers/GalleryDataContext';
 import GalleryItem from './GalleryItem';
 import PortfolioPopup from './PortfolioPopup';
 import Showcase from './Showcase';
+import VideoShowcase from './VideoShowcase';
 
 const Gallery = ({disableScrollHandler}) => {
   const { galleryData } = useContext(GalleryDataContext);
   const [showcaseActive, setShowcaseActive] = useState({class: ''});
+  const [videoShowcaseActive, setVideoShowcaseActive] = useState({class: ''});
+
   const defultPopupData = {  
     title: '',
     tools: [],
@@ -40,15 +43,27 @@ const Gallery = ({disableScrollHandler}) => {
     setShowcaseActive({...showcaseActive, class: ''});
   }
 
+  function videoShowcaseOpen () {
+    disableScrollHandler();
+    setVideoShowcaseActive({...showcaseActive, class: 'active'});
+  }
+  function videoShowcaseClose () {
+    disableScrollHandler();
+    setVideoShowcaseActive({...showcaseActive, class: ''});
+  }
+
   return (
     <div className="gallery container-fluid" id="portfolioGallery">
       <div className="row">
         <ul  className="col-12 gridBox">
           {galleryData.map((data) => (
-            <GalleryItem key={data.id} project={data} openDetails={openPopup}  />
+            <GalleryItem key={data.id} project={data} openDetails={videoShowcaseOpen}  />
             ))}
         </ul>
+        {/* <Showcase options={videoShowcaseActive} showcaseClose={videoShowcaseClose}/> */}
+        <VideoShowcase options={videoShowcaseActive} showcaseClose={videoShowcaseClose}/>
         <Showcase options={showcaseActive} showcaseClose={showcaseClose}/>
+
         <PortfolioPopup showPopup={portfolioPopupSettings.showPopup} closeFunction={hidePopup}  data={portfolioPopupSettings.data}/>
       </div>
     </div>

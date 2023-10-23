5789376233135ec1cba3a236726c28e5b0d2859a
Igoris Ivanovas <igoris.x@inbox.lt>
2023-10-23 18:52:03 +0200

    BlogItem setup

---
+++ src/components/BlogSection/BlogItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogItem() {
+  return (
+    <div className='blogItem'>
+      BlogItem
+    </div>
+  )
+}
+
+export default BlogItem
--- src/components/BlogSection/BlogSection.jsx
+++ src/components/BlogSection/BlogSection.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../Base/Header";
-import { BiBookAlt } from 'react-icons/bi';
-
+import { BiBookAlt } from "react-icons/bi";
+import BlogItem from "./BlogItem";
 
 function BlogSection() {
-  return <section className="blogSection"><Header text='My Blogs.' icon={<BiBookAlt/>}/></section>;
+  return (
+    <section className="blogSection">
+      <Header text="My Blogs." icon={<BiBookAlt />} />
+      <BlogItem />
+      
+    </section>
+  );
 }
 
 export default BlogSection;

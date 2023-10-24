56874723f7555ac06eed0850194435759269e885
Igoris Ivanovas <igoris.x@inbox.lt>
2023-10-24 19:21:05 +0200

    secondaryTitle dinamick font color

--- src/components/BlogSection/BlogItem.jsx
+++ src/components/BlogSection/BlogItem.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import OptionsContext from '../../reducers/OptionsContext';
+
 
 function BlogItem() {
+  const { optionsState } = useContext(OptionsContext);
+  console.log(optionsState);
   return (
     <div className='blogItem col-12 col-sm-6 col-lg-4'>
       <img src="%" alt="#" />
       <div className='blogItemContent'>
-        <span className='cat'>Web desing</span>
+        <span className='secondaryTitle sectionParagraph' style={{color: optionsState.color}}>Web desing</span>
         <h4 cl className="blogTitle sectionSecondaryHeader">Blog title</h4>
         <div className="blogDate sectionParagraph">2000.02.21</div>
       </div>

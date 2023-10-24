20fdbd55c830db45bc0d4a19cf2df0d800cc718d
Igoris Ivanovas <igoris.x@inbox.lt>
2023-10-24 19:38:01 +0200

    blogItem css

--- src/components/BlogSection/BlogItem.jsx
+++ src/components/BlogSection/BlogItem.jsx
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react'
-import OptionsContext from '../../reducers/OptionsContext';
-
+import React, { useContext } from "react";
+import OptionsContext from "../../reducers/OptionsContext";
 
 function BlogItem() {
   const { optionsState } = useContext(OptionsContext);
   console.log(optionsState);
   return (
-    <div className='blogItem col-12 col-sm-6 col-lg-4'>
-      <img src="%" alt="#" />
-      <div className='blogItemContent'>
-        <span className='secondaryTitle sectionParagraph' style={{color: optionsState.color}}>Web desing</span>
-        <h4 cl className="blogTitle sectionSecondaryHeader">Blog title</h4>
+    <div className="blogItem col-12 col-sm-6 col-lg-4">
+      <div className="img">
+        <img src="%" alt="#" />
+      </div>
+      <div className="blogItemContent">
+        <span
+          className="secondaryTitle sectionParagraph"
+          style={{ color: optionsState.color }}
+        >
+          Web desing
+        </span>
+        <h4 cl className="blogTitle sectionSecondaryHeader">
+          Best Way to Design
+        </h4>
         <div className="blogDate sectionParagraph">2000.02.21</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default BlogItem
+export default BlogItem;

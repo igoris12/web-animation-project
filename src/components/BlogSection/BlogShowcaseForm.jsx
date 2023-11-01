ace3c3b9506cee68009c9b82645bb83c031c04cc
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-01 17:51:56 +0100

    blogShowcaseForm layout

--- src/components/BlogSection/BlogShowcaseForm.jsx
+++ src/components/BlogSection/BlogShowcaseForm.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Button from "../Base/Button";
 
 function BlogShowcaseForm() {
   return (
-    <div className='blogShowcaseForm'>
-        from
+    <div className="blogShowcaseForm">
+      <h4 className="sectionSecondaryHeader">Leave a reply</h4>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <Button text='Post comment' />
+      </form>
     </div>
-  )
+  );
 }
 
-export default BlogShowcaseForm
+export default BlogShowcaseForm;

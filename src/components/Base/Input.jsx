f878cfb64032ce699426b4bec8fdc6b2fbdef76b
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-04 11:48:36 +0100

    textarea inout  bug fixed

--- src/components/Base/Input.jsx
+++ src/components/Base/Input.jsx
@@ -1,11 +1,19 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 
 function Input({ name, type = "text", placeholder = "input", width = "100%" }) {
-  const [input , setInput] = useState('')
+  const [input, setInput] = useState("");
 
   return (
-    <div className={ input.length === 0 ? "input" :"input focus"} style={{ width: width }}>
-      <input onChange={(e)=> setInput(e.target.value)} type={type} placeholder={placeholder} name={name} />
+    <div
+      className={input.length === 0 ? "input" : "input focus"}
+      style={{ width: width }}
+    >
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        type={type}
+        placeholder={placeholder}
+        name={name}
+      />
       <label>{placeholder}</label>
     </div>
   );

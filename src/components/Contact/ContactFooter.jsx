4c6e1459ddf0ed46fa89193fb3dfd8a1be9682bf
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-11 20:06:29 +0100

    create and footer

---
+++ src/components/Contact/ContactFooter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ContactFooter = () => {
+  return (
+    <footer className="contactFooter">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35945.881180821896!2d26.203878433059277!3d55.73039793827166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46c2ba89a59d97f3%3A0x5328d68c690dff0b!2sZarasai%2C%20Zarasai%20District%20Municipality%2C%20Lithuania!5e0!3m2!1sen!2sbe!4v1699729259193!5m2!1sen!2sbe"
+        width="100%"
+        height="100"
+        loading="lazy"
+        style={{border: '0'}}
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      
+    </footer>
+  );
+};
+
+export default ContactFooter;

0362e42dd5880b2ecc6334e479d6ba46ae4bfd47
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-11 16:13:30 +0100

    form layout rework

--- src/components/Contact/ContactFrom.jsx
+++ src/components/Contact/ContactFrom.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import SectionHeader from '../Base/SectionHeader'
+import React from "react";
+import SectionHeader from "../Base/SectionHeader";
+import Input from "../Base/Input";
+import Textarea from "../Base/Textarea";
+import Button from "../Base/Button";
 
 function ContactFrom() {
   return (
-    <div className='contactFrom'>
-      <SectionHeader text={"Let's talk"}/>
-        <form>
-          <input type="text" />
-          <input type="text" />
-          <textarea id="" cols="30" rows="10"></textarea>
-          <button>Send massage</button>
-        </form>
+    <div className="contactFrom">
+      <SectionHeader text={"Let's talk"} />
+      <form>
+       
+        <div className="input">
+        <Input  name="name" placeholder="Name"/>
+        </div>
+        <div className="input">
+        <Input  name="email" placeholder="Email"/>
+        </div>
+        <div className="commentarea">
+        <Textarea placeholder='How can we help you?' />
+        </div>
+       <Button text={'Send massage'} width="200px" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default ContactFrom
+export default ContactFrom;

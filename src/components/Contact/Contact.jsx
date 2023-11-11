afb2eef49ecb4b658f5508d6f860ce09c4048bc4
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-11 15:49:45 +0100

    Contact form layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,15 @@ function Contact() {
   return (
     <section className='contact'>
        <Header text={'Contact me.'} icon={<HiOutlineMail/>}/>
+       <div className="form">
+        <h4>Let's talk</h4>
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <textarea id="" cols="30" rows="10"></textarea>
+          <button>Send massage</button>
+        </form>
+       </div>
     </section>
   )
 }

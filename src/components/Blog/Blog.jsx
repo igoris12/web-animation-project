053b07ffa589925ecd14116b6301cc34cce7b7b4
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-12 15:16:14 +0100

    Revert "Navigation"

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
 import AboutMe from "../AboutMe/AboutMe";
 import Home from "../Home/Home";
 import Portfolio from "../Portfolio/Portfolio";
@@ -7,18 +6,15 @@ import Resume from "../Resume/Resume";
 import BlogSection from "../BlogSection/BlogSection";
 import Contact from "../Contact/Contact";
 
-
 const Blog = () => {
   return (
     <section className="blog-page ">
-      <Routes>
-        <Route path="/web-animation-project/" element={<Home />} />
-        <Route path="/web-animation-project/aboutMe" element={<AboutMe />} />
-        <Route path="/web-animation-project/resume" element={<Resume />} />
-        <Route path="/web-animation-project/portfolio" element={<Portfolio />} />
-        <Route path="/web-animation-project/blogSection" element={<BlogSection />} />
-        <Route path="/web-animation-project/contact" element={<Contact />} />
-      </Routes>
+      {/* <Home /> */}
+      {/* <AboutMe /> */}
+      {/* <Resume /> */}
+      {/* <Portfolio /> */}
+      {/* <BlogSection /> */}
+      <Contact/>
     </section>
   );
 };
